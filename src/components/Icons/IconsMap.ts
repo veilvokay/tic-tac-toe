a9f9180a5caabb433ea675093001ab6e9e4185d1
Roman Yakovliev <54674847+veilvokay@users.noreply.github.com>
2022-09-20 03:03:25 +0200

    [WIP]: Gameplay page

--- src/components/Icons/IconsMap.ts
+++ src/components/Icons/IconsMap.ts
@@ -1,7 +1,9 @@
 import { ReactComponent as IconCircle } from '../../assets/icons/icon-circle.svg';
 import { ReactComponent as IconCross } from '../../assets/icons/icon-cross.svg';
+import { ReactComponent as IconReset } from '../../assets/icons/icon-reset.svg';
 
 export const IconsMap = {
     IconCircle,
     IconCross,
+    IconReset,
 }
--- src/pages/GamePage/index.tsx
+++ src/pages/GamePage/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import './styles.scss'
+import React, { useContext } from 'react'
+import { Icon } from '../../components/Icons/Icon'
+import { IconsMap } from '../../components/Icons/IconsMap'
 import Container from '../../components/layout/Container'
 import Page from '../../components/layout/Page'
+import { PlayerContext } from '../../constants/Player'
 import { GameType } from '../../constants/routes'
 
 interface Props {
@@ -10,12 +14,39 @@ interface Props {
 const GamePage = (props: Props) => {
     const { gameType } = props;
 
+    const playerCtx = useContext(PlayerContext);
 
     return (
-        <Page id="home-page" className="home-page">
+        <Page id="game-page" className="game-page">
             <Container>
-                <div className="home-page__content">
-                    
+                <div className="game-page__content">
+                    <div className="game-page__menu">
+                        <div className="game-page__menu-icons">
+                            <Icon
+                                className="icon-cross"
+                                content={<IconsMap.IconCross />}
+                            />
+                            <Icon
+                                className="icon-circle"
+                                content={<IconsMap.IconCircle />}
+                            />
+                        </div>
+                        <div className="game-page__menu-turn block">
+                            Turn
+                        </div>
+                        <button className="button game-page__menu-reset flex">
+                            <Icon
+                                className="icon-reset"
+                                content={<IconsMap.IconReset />}
+                            />
+                        </button>
+                    </div>
+                    <div className="game-page__gamefield">
+
+                    </div>
+                    <div className="game-page__score">
+
+                    </div>
                 </div>
             </Container>
         </Page>
